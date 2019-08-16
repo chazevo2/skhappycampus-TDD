d2285b0b8d515e48d9bbfc241f85c00c2bc8be4b
Junhyuk Park <chazevo2@gmail.com>
2019-08-16 14:25:04 +0900

    create project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # skhappycampus-TDD
-Calculator in TDD
+Calculator in TDD(Test Driven Development)
+Using JUnit vers-4.12
---
+++ calculator/src/test/java/com/example/calculator/CalculatorTest.java
@@ -0,0 +1,8 @@
+package com.example.calculator;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+
+    }
+}
